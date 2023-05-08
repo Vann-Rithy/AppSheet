d355b6c538f9b06be4fceeeea11f59739656b54f
CGRITHY <ahpea88@gmail.com>
2023-05-08 12:44:02 +0700

    new size

--- src/Home.js
+++ src/Home.js
@@ -30,8 +30,8 @@ export default function EnterCode() {
 			  type: "LiveStream",
 			  target: document.querySelector("#scanner-container"),
 			  constraints: {
-				width: 600,
-				height: 400,
+				width: 500,
+				height: 200,
 				facingMode: "environment",
 			  },
 			},
