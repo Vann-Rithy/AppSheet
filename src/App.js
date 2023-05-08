315c663979ace38e58970fdc0548d59a41c18b54
CGRITHY <ahpea88@gmail.com>
2023-05-08 12:28:08 +0700

    addcss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import Home from './Home'
+import Home from './Home';
+import'./App.css'
+
 // import Scanner from './page/Scanner'
 export default function App() {
   return (
